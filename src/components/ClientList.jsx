c2b0367c1ddd188286e7493316b4c1e36f417cf1
Oliv <oallardjacquin@gmail.com>
2019-11-29 16:46:52 +0100

    save

---
+++ src/components/ClientList.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { NestorContext } from '../providers/Provider';
+
+const ClientList = () =>
+{
+	const { clients } = useContext(NestorContext);
+
+	return clients.map((client, i) =>
+	{
+		return (
+			<div key={i}>
+				<div>{client.name}</div>
+				<div>eMail : {client.email}</div>
+				<div>Phone : {client.phone}</div>
+				<div>Birth Date : {client.birthDate}</div>
+				<div>Nationality : {client.nationality}</div>
+				<div>Status : {client.status}</div>
+				<button></button>
+			</div>
+		);
+	});
+
+
+}
+
+export default ClientList;
