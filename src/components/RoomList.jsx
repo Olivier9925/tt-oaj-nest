6dbb25be3307b055790da983a0477ea1be720a57
Oliv <oallardjacquin@gmail.com>
2019-11-29 21:17:45 +0100

    ok

---
+++ src/components/RoomList.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+
+class RoomList extends Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {}
+	}
+	render()
+	{
+		const { rooms } = this.props;
+
+		return rooms.map((room, i) =>
+		{
+			return (
+				<div key={i}>
+					<div>{room.name}</div>
+					<div>Address : {room.address}</div>
+					<div>city : {room.city}</div>
+				</div>
+			);
+		});
+
+	}
+}
+
+export default RoomList;
